b5b2c31e916f8c9165c506d208c5b6ab20755c3f
Alex Tercete <alextercete@gmail.com>
2016-07-08 14:40:37 +0100

    Moves PerishableItem tests into separate file

--- src/GildedRose.Tests/ItemUpdaterTests.cs
+++ src/GildedRose.Tests/ItemUpdaterTests.cs
@@ -7,39 +7,6 @@ namespace GildedRose.Tests
     public class ItemUpdaterTests
     {
         [Test]
-        public void StandardItemShouldLowerQualityAndSellInByOne()
-        {
-            var item = new PerishableItem { Name = "+5 Dexterity Vest", SellIn = 3, Quality = 6 };
-
-            UpdateItem(item);
-
-            Assert.AreEqual(5, item.Quality);
-            Assert.AreEqual(2, item.SellIn);
-        }
-
-        [Test]
-        public void StandardItemShouldLowerQualityTwiceAsFastWhenSellInIsNegative()
-        {
-            var item = new PerishableItem { Name = "+5 Dexterity Vest", SellIn = -2, Quality = 6 };
-
-            UpdateItem(item);
-
-            Assert.AreEqual(4, item.Quality);
-            Assert.AreEqual(-3, item.SellIn);
-        }
-
-        [Test]
-        public void StandardItemShouldLowerQualityTwiceAsFastWhenSellInIsZero()
-        {
-            var item = new PerishableItem { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 6 };
-
-            UpdateItem(item);
-
-            Assert.AreEqual(4, item.Quality);
-            Assert.AreEqual(-1, item.SellIn);
-        }
-
-        [Test]
         public void BackstagePassItemShouldIncreaseQualityTwiceAsFastWhenSellInLessThanElevenDays()
         {
             var item = new DesirableEventItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 6 };
@@ -81,17 +48,6 @@ namespace GildedRose.Tests
 
             Assert.AreEqual(8, item.Quality);
             Assert.AreEqual(-1, item.SellIn);
-        }
-
-        [Test]
-        public void StandardItemQualityIsNeverNegative()
-        {
-            var item = new PerishableItem { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 };
-
-            UpdateItem(item);
-
-            Assert.AreEqual(0, item.Quality);
-            Assert.AreEqual(9, item.SellIn);
         }
 
         [Test]
---
+++ src/GildedRose.Tests/PerishableItemTests.cs
@@ -0,0 +1,52 @@
+﻿using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    public class PerishableItemTests
+    {
+        [Test]
+        public void StandardItemShouldLowerQualityAndSellInByOne()
+        {
+            var item = new PerishableItem { Name = "+5 Dexterity V333est", SellIn = 3, Quality = 6 };
+
+            item.Update();
+
+            Assert.AreEqual(5, item.Quality);
+            Assert.AreEqual(2, item.SellIn);
+        }
+
+        [Test]
+        public void StandardItemShouldLowerQualityTwiceAsFastWhenSellInIsNegative()
+        {
+            var item = new PerishableItem { Name = "+5 Dexterity Vest", SellIn = -2, Quality = 6 };
+
+            item.Update();
+
+            Assert.AreEqual(4, item.Quality);
+            Assert.AreEqual(-3, item.SellIn);
+        }
+
+        [Test]
+        public void StandardItemShouldLowerQualityTwiceAsFastWhenSellInIsZero()
+        {
+            var item = new PerishableItem { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 6 };
+
+            item.Update();
+
+            Assert.AreEqual(4, item.Quality);
+            Assert.AreEqual(-1, item.SellIn);
+        }
+
+        [Test]
+        public void StandardItemQualityIsNeverNegative()
+        {
+            var item = new PerishableItem { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 };
+
+            item.Update();
+        
+            Assert.AreEqual(0, item.Quality);
+            Assert.AreEqual(9, item.SellIn);
+        }
+    }
+}
