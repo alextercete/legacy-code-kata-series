ba4dd8cd17a727356452d2fd24f66ad7d2adeeda
Alex Tercete <alextercete@gmail.com>
2016-07-08 13:59:03 +0100

    Introduce Item implementations

--- src/GildedRose.Console/Item.cs
+++ src/GildedRose.Console/Item.cs
@@ -1,11 +1,172 @@
 namespace GildedRose.Console
 {
-    public class Item
+    public interface Item
+    {
+        string Name { get; set; }
+
+        int SellIn { get; set; }
+
+        int Quality { get; set; }
+
+        void Update();
+    }
+
+    public class AgeingItem : Item
     {
         public string Name { get; set; }
-
         public int SellIn { get; set; }
-
         public int Quality { get; set; }
+
+        public void Update()
+        {
+            IncreaseQuality();
+            DecreaseSellIn();
+
+            if (HasPassedSellByDate())
+            {
+                IncreaseQuality();
+            }
+        }
+
+        private bool HasPassedSellByDate()
+        {
+            return this.SellIn < 0;
+        }
+
+        private void IncreaseQuality()
+        {
+            if (this.Quality < 50)
+            {
+                this.Quality = this.Quality + 1;
+            }
+        }
+
+        private void DecreaseSellIn()
+        {
+            this.SellIn = this.SellIn - 1;
+        }
+    }
+
+    public class DesirableEventItem : Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void Update()
+        {
+            // Tickets are more valuable when an event is closer
+            if (this.SellIn <= 10)
+            {
+                IncreaseQuality();
+            }
+
+            // They increase in value much more the closer we are to the event
+            if (this.SellIn <= 5)
+            {
+                IncreaseQuality();
+            }
+
+            IncreaseQuality();
+            DecreaseSellIn();
+
+            if (HasPassedSellByDate())
+            {
+                this.Quality = 0;
+            }
+        }
+
+        private bool HasPassedSellByDate()
+        {
+            return this.SellIn < 0;
+        }
+
+        private void IncreaseQuality()
+        {
+            if (this.Quality < 50)
+            {
+                this.Quality = this.Quality + 1;
+            }
+        }
+
+        private void DecreaseSellIn()
+        {
+            this.SellIn = this.SellIn - 1;
+        }
+    }
+
+    public class LegendaryItem : Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void Update()
+        {
+
+        }
+    }
+
+    public class ConjuredItem : Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void Update()
+        {
+            DecreaseQuality();
+            DecreaseQuality();
+            DecreaseSellIn();
+        }
+
+        private void DecreaseSellIn()
+        {
+            this.SellIn = this.SellIn - 1;
+        }
+
+        private void DecreaseQuality()
+        {
+            if (this.Quality > 0)
+            {
+                this.Quality = this.Quality - 1;
+            }
+        }
+    }
+
+    public class PerishableItem : Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void Update()
+        {
+            DecreaseQuality();
+            DecreaseSellIn();
+
+            if (HasPassedSellByDate())
+            {
+                DecreaseQuality();
+            }
+        }
+
+        private bool HasPassedSellByDate()
+        {
+            return this.SellIn < 0;
+        }
+
+        private void DecreaseSellIn()
+        {
+            this.SellIn = this.SellIn - 1;
+        }
+
+        private void DecreaseQuality()
+        {
+            if (this.Quality > 0)
+            {
+                this.Quality = this.Quality - 1;
+            }
+        }
     }
 }
--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -23,17 +23,17 @@ namespace GildedRose.Console
             {
                 Items = new List<Item>
                 {
-                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                    new Item
+                    new PerishableItem {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new AgeingItem {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new PerishableItem {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new LegendaryItem {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new DesirableEventItem
                     {
                         Name = "Backstage passes to a TAFKAL80ETC concert",
                         SellIn = 15,
                         Quality = 20
                     },
-                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                    new ConjuredItem {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
                 }
             };
 
@@ -59,109 +59,9 @@ namespace GildedRose.Console
         {
             foreach (var item in items)
             {
-                if (item.Name == "Aged Brie")
-                {
-                    UpdateAgeingItem(item);
-                }
-                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    UpdateDesirableEventItem(item);
-                }
-                else if (item.Name == "Sulfuras, Hand of Ragnaros")
-                {
-                    UpdateLegendaryItem(item);
-                }
-                else if (item.Name == "Conjured Mana Cake")
-                {
-                    UpdateConjuredItem(item);
-                }
-                else
-                {
-                    UpdatePerishableItem(item);
-                }
+                item.Update();
             }
         }
 
-        private static void UpdateConjuredItem(Item item)
-        {
-            DecreaseQuality(item);
-            DecreaseQuality(item);
-            DecreaseSellIn(item);
-        }
-
-        private static void UpdateAgeingItem(Item item)
-        {
-            IncreaseQuality(item);
-            DecreaseSellIn(item);
-
-            if (HasPassedSellByDate(item))
-            {
-                IncreaseQuality(item);
-            }
-        }
-
-        private static void UpdateDesirableEventItem(Item item)
-        {
-            // Tickets are more valuable when an event is closer
-            if (item.SellIn <= 10)
-            {
-                IncreaseQuality(item);
-            }
-
-            // They increase in value much more the closer we are to the event
-            if (item.SellIn <= 5)
-            {
-                IncreaseQuality(item);
-            }
-
-            IncreaseQuality(item);
-            DecreaseSellIn(item);
-
-            if (HasPassedSellByDate(item))
-            {
-                item.Quality = 0;
-            }
-        }
-
-        private static void UpdateLegendaryItem(Item item)
-        {
-        }
-
-        private static void UpdatePerishableItem(Item item)
-        {
-            DecreaseQuality(item);
-            DecreaseSellIn(item);
-
-            if (HasPassedSellByDate(item))
-            {
-                DecreaseQuality(item);
-            }
-        }
-
-        private static bool HasPassedSellByDate(Item item)
-        {
-            return item.SellIn < 0;
-        }
-
-        private static void IncreaseQuality(Item item)
-        {
-            if (item.Quality < 50)
-            {
-                item.Quality = item.Quality + 1;
-            }
-        }
-
-        private static void DecreaseQuality(Item item)
-        {
-            if (item.Quality > 0)
-            {
-                item.Quality = item.Quality - 1;
-            }
-        }
-
-        private static void DecreaseSellIn(Item item)
-        {
-            item.SellIn = item.SellIn - 1;
-        }
     }
 }
